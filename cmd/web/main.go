8fba5ca01eb6705be3b80d6b0e7bc5d1fe891c76
ono5 <kuroneko_head_lord@yahoo.co.jp>
2021-03-27 08:12:06 +0900

    Improving our WS endpoint, and setting up goroutines

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"chatapp/internal/handlers"
 	"log"
 	"net/http"
 )
 
 func main() {
 	mux := routes()
+
+	log.Println("Starting channel listener")
+	go handlers.ListenToWsChannel()
+
 	log.Println("Starting web server on port 8080")
 
 	_ = http.ListenAndServe(":8080", mux)
