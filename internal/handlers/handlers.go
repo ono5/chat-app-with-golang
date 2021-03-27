2d6eb238602be554d2ba9b0bdbf89606410e89e7
ono5 <kuroneko_head_lord@yahoo.co.jp>
2021-03-28 07:12:24 +0900

    fix: when emptying username, skip to add useername into list

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -126,7 +126,9 @@ func ListenToWsChannel() {
 func getUserList() []string {
 	var userList []string
 	for _, x := range clients {
-		userList = append(userList, x)
+		if x != "" {
+			userList = append(userList, x)
+		}
 	}
 	sort.Strings(userList)
 	return userList
